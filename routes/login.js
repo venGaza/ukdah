07264d72761670b25fc391502138acff30a1633c
santokis <santokis@oregonstate.edu>
2019-02-13 00:32:23 -1000

    POST values & sqlite
    Added simple form entry validation and continuing to build login functionality.

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+function isEmpty (obj) {
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('login', { 
@@ -8,18 +16,37 @@ router.get('/', function(req, res, next) {
 	});
 });
 
-//ss021119
-router.post('/', function(req, res, next){
+//post request
+router.post('/', function(req, res, next) {
+	var sqlite3 = req.app.get('sqlite3');
 	var newUser = {
 		email: req.body.email,
 		password: req.body.password
 	}
 
+	//console.log(newUser);
 	console.log(newUser.email);
 	console.log(newUser.password);
 
-	//validation here
+	//let sql = `SELECT [userID], [email]`;
+	let sql = `SELECT [userID]`;
+	sql += `FROM user WHERE email=?`;
 
+	sqlite3.db.all(sql, [newUser.email], (error, results) => {
+		if (error) {
+            results.write(JSON.stringify(error));
+            results.end();
+        } else if (isEmpty(results)) { //
+        	console.log("Email not found");
+        } else {
+        	console.log("Email found");
+        	console.log(results);
+        }
+
+		//console.log(results[0].userID);
+	});
+	
+	sqlite3.db.close()
 });
 
 module.exports = router;
