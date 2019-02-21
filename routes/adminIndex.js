447697b59d61065e6eb94a55de293ebedd76990e
santokis <santokis@oregonstate.edu>
2019-02-20 19:32:16 -1000

    Sessions: user specific pages
    Awards page: accessible by user (userTypeID: 1).
    Admin console: accessible by admin (userTypeID: 2).

--- routes/adminIndex.js
+++ routes/adminIndex.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET admin index page. */
+function checkUserType(res, sqlite3, sql, adminUser){
+    sqlite3.db.all(sql, [], (error, results) => {
+        if(error){
+            res.write(JSON.stringify(error));
+            res.end();
+        } else if (results[0].userTypeID == adminUser) {
+        	console.log("admin user");
+            res.render('adminIndex');
+        } else {
+            console.log("normal user, unable to access admin console");
+            res.redirect('/userIndex');
+        }
+    });
+}
+
 router.get('/', function(req, res, next) {
-  res.render('adminIndex', { title: 'Ukdah |  Employee Recognition Portal' });
+	//assume session is valid
+	var sqlite3 = req.app.get('sqlite3');
+	var adminUser = 2;
+
+	let query = `SELECT userTypeID `;
+	query += `FROM user `;
+	query += `WHERE email='` + global.email + `'`;
+
+	checkUserType(res, sqlite3, query, adminUser);
 });
 
 module.exports = router;
--- routes/createAward.js
+++ routes/createAward.js
@@ -1,8 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+function checkUserType(res, sqlite3, sql, adminUser){
+    sqlite3.db.all(sql, [], (error, results) => {
+        if(error){
+            res.write(JSON.stringify(error));
+            res.end();
+        } else if (results[0].userTypeID == adminUser) {
+        	console.log("admin user, unable to access awards");
+        	res.redirect('/userIndex');
+        } else {
+            console.log("normal user");
+            res.render('createAward');
+        }
+    });
+}
+
 router.get('/', function(req, res) {
-	res.render('createAward');
+	//assume session is valid
+	var sqlite3 = req.app.get('sqlite3');
+	var adminUser = 2;
+
+	let query = `SELECT userTypeID `;
+	query += `FROM user `;
+	query += `WHERE email='` + global.email + `'`;
+
+	checkUserType(res, sqlite3, query, adminUser);
 });
 
 module.exports = router;
