9286f0f16ba974d60dfb3c399c025a0940b1a802
Emmet Cooke <cookee@oregonstate.edu>
2019-02-28 21:16:07 -0600

    Added initial CSV file functionality

---
+++ public/javascripts/createCSV.js
@@ -0,0 +1,41 @@
+function download(filename, text) {
+    var file = new File([text], filename);
+    var element = document.createElement('a');
+    element.setAttribute('href', URL.createObjectURL(file));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+function createCSV(data) {
+    csv = "";
+
+    for (i = 0; i < Object.keys(data[0]).length; i++) {
+        if (i === Object.keys(data[0]).length - 1) {
+            csv += Object.keys(data[0])[i] + '\n';
+        }
+        else {
+            csv += Object.keys(data[0])[i] + ',';
+        }
+    }
+
+    for (i = 0; i < data.length; i++) {
+        for (j = 0; j < Object.values(data[i]).length; j++) {
+            if (j === Object.values(data[i]).length - 1) {
+                csv += Object.values(data[i])[j] + '\n';
+            }
+            else {
+                csv += Object.values(data[i])[j] + ',';
+            }
+        }
+    }
+
+    document.getElementById('premadeExport').addEventListener("click", function () {
+        download("data.csv", csv);
+    });
+}
--- routes/adminBusiness.js
+++ routes/adminBusiness.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
+function getUsers(res, sqlite3, sql, context, complete) {
+    sqlite3.db.all(sql, [], (error, results) => {
+        var length = 0;
+        if (error) {
+            res.write(JSON.stringify(error));
+            res.end();
+        }
+        results.forEach((result) => {
+            length++;
+        });
+        var users = new Array(length);
+        for (i = 0; i < length; i++) {
+            users[i] = results[i];
+        }
+        context.createAward = JSON.stringify(users);
+        context.users = results;
+        complete();
+    });
+};
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('adminBusiness');
+    var callbackCount = 0;
+    var context = {};
+    var sqlite3 = req.app.get('sqlite3');
+    let sql4 = `SELECT fname as First_Name, lname as Last_Name, COUNT(userAwards.empID) as Num_Awards FROM user
+				JOIN userAwards
+				ON user.userID = userAwards.userID
+				GROUP BY userAwards.userID
+				ORDER BY Num_Awards DESC;`
+    getUsers(res, sqlite3, sql4, context, complete);
+    function complete() {
+        callbackCount++;
+        if (callbackCount >= 1) {
+            res.render('adminBusiness', context);
+        }
+    }
 });
 
 module.exports = router;
